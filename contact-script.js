8a63c9e238e4e17c99c5e9197f6472acd7ba397b
am2002 <44576253+am2002@users.noreply.github.com>
2018-12-06 20:04:22 +0100

    big update

---
+++ contact-script.js
@@ -0,0 +1,8 @@
+var naam = "";
+var email = "";
+function idonutknow()
+  {
+    var naam = document.getElementById("naam").value;
+    var email = document.getElementById("email").value;
+    document.getElementById("contactlink").href="google.com"; 
+  }
